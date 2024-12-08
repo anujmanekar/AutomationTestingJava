65f96e03599ea2ee31a494881273ebfc3738c8e7
anujmanekar <anujmanekar96@gmail.com>
2024-12-08 17:08:10 +0530

    Add the files

--- src/November/ex_24_11_2024/Lab001.java
+++ src/November/ex_24_11_2024/Lab001.java
@@ -3,8 +3,12 @@ package November.ex_24_11_2024;
 public class Lab001
 {
     public static void main(String[] args) {
-        System.out.println("djfskdjfsdklfjskfjsnlkj");
-        System.out.println("fesdfsf");
+        System.out.println("This is my first java program");
+        System.out.println("HELLO");
+        System.out.println("NOvemeber");
+        System.out.println("New");
+        System.out.println("Program");
+        System.out.println("Test");
     }
 
 
