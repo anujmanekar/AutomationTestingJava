8f70939f4f063f2568dd08d0a1d3396b9a4ff827
anujmanekar <anujmanekar96@gmail.com>
2025-01-08 13:52:18 +0530

    Output

--- src/January_2025/ex_07_01_2025/Automation_String.java
+++ src/January_2025/ex_07_01_2025/Automation_String.java
@@ -3,7 +3,7 @@ package January_2025.ex_07_01_2025;
 public class Automation_String {
 
     public static void main(String[] args) {
-        String name= "Testing";
-        System.out.printf("My Name is Anuj and I am watching the Automation %s videos", name);
+        int a= 10;
+        System.out.printf("I want to print %d", a);
     }
 }
