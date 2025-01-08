4043be1c919088d5b2c1e547d3f823b24428919f
anujmanekar <anujmanekar96@gmail.com>
2025-01-08 22:29:57 +0530

    Table of 10

--- src/January_2025/ex_07_01_2025/Automation_String.java
+++ src/January_2025/ex_07_01_2025/Automation_String.java
@@ -4,6 +4,18 @@ public class Automation_String {
 
     public static void main(String[] args) {
         int a= 10;
-        System.out.printf("I want to print %d", a);
+        System.out.printf("This is the table of 10\n");
+        System.out.printf("%d * %d = %d\n", a, 1, a*1);
+        System.out.printf("%d * %d = %d\n", a, 1, a*2);
+        System.out.printf("%d * %d = %d\n", a, 1, a*3);
+        System.out.printf("%d * %d = %d\n", a, 1, a*4);
+        System.out.printf("%d * %d = %d\n", a, 1, a*5);
+        System.out.printf("%d * %d = %d\n", a, 1, a*6);
+        System.out.printf("%d * %d = %d\n", a, 1, a*7);
+        System.out.printf("%d * %d = %d\n", a, 1, a*8);
+        System.out.printf("%d * %d = %d\n", a, 1, a*9);
+        System.out.printf("%d * %d = %d\n", a, 1, a*10);
+
+
     }
 }
