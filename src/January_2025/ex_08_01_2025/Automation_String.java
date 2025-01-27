050bd31f32c7a8b65f6564d696c423d5cca3a317
anujmanekar <anujmanekar96@gmail.com>
2025-01-27 14:33:12 +0530

    Calculator

--- src/January_2025/ex_08_01_2025/Automation_String.java
+++ src/January_2025/ex_08_01_2025/Automation_String.java
@@ -1,9 +1,13 @@
 package January_2025.ex_08_01_2025;
 
 public class Automation_String {
-    {
-
-
-
-}
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 32;
+        System.out.println(a + c);
+        System.out.println(c - b);
+        System.out.println(c * b);
+        System.out.println(a / b);
+    }
 }
