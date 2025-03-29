81561660e85e93075d3bfbb490a5362408720845
anujmanekar <anujmanekar96@gmail.com>
2025-03-29 17:54:54 +0530

    Calculator

---
+++ src/March_2025/string_print.java
@@ -0,0 +1,21 @@
+package March_2025;
+
+public class string_print {
+    public static void main(String[] args) {
+        int a=10;
+        int h=2;
+        int t=4;
+        int k=5;
+        int q=6;
+        int p=87;
+        int m=43;
+        int i=89;
+
+        System.out.println(a-k);
+        System.out.println(p-m);
+        System.out.println(a-i);
+        System.out.println(h/i);
+        System.out.println(k-t);
+        System.out.println(t*k);
+    }
+}
