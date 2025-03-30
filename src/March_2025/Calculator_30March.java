14ba0ba02b95c1ecc8ab6d046b70271c631f0e9d
anujmanekar <anujmanekar96@gmail.com>
2025-03-30 21:10:18 +0530

    Calculator

--- src/March_2025/Calculator_30March.java
+++ src/March_2025/Calculator_30March.java
@@ -3,8 +3,8 @@ package March_2025;
 
 public class Calculator_30March {
 
-    public static void main(String[] args) {
-        System.out.println("Hi I am Anuj");
+    public static void main(String[] anuj) {
+        System.out.println("Hi I am xcas");
         System.out.println(2+2);
         System.out.println(2-2);
     }
