8085678d5163a013ce2e18957397f9703c58ce22
anujmanekar <anujmanekar96@gmail.com>
2025-03-30 15:05:11 +0530

    Calculator

---
+++ src/March_2025/Calculator_30March.java
@@ -0,0 +1,11 @@
+package March_2025;
+
+
+public class Calculator_30March {
+
+    public static void main(String[] args) {
+        System.out.println("Hi I am Anuj");
+        System.out.println(2+2);
+        System.out.println(2-2);
+    }
+}
--- src/March_2025/March_30.java
+++
@@ -1,9 +0,0 @@
-package March_2025;
-
-public class March_30 {
-
-        public static void main(String [] args){
-            System.out.println("Testing automation");
-        }
-    }
-
