55e3053f34316aae92491f5b0c073ee8937b9f85
anujmanekar <anujmanekar96@gmail.com>
2025-03-31 10:30:57 +0530

    Calculator

--- src/March_2025/Variables_datatypes.java
+++ src/March_2025/Variables_datatypes.java
@@ -3,6 +3,7 @@ package March_2025;
 public class Variables_datatypes {
     public static void main(String[] args) {
         int age = 10;
-        System.out.println(age);
+        int num = 321;
+        System.out.println(num);
     }
 }
