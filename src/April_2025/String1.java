a38d6a61a57c2bb5efed1a65692f1570116276dc
anujmanekar <anujmanekar96@gmail.com>
2025-04-01 22:54:03 +0530

    Calculator

---
+++ src/April_2025/String1.java
@@ -0,0 +1,17 @@
+package April_2025;
+
+public class String1 {
+    public static void main(String1[] args) {
+        String name = "Anuj";
+        String Surname = "Manekar";
+        String Middle = "Rajendra";
+        System.out.println("Name "+ name);
+        System.out.println("Middle "+ Middle);
+        System.out.println("Surname "+ Surname);
+
+
+
+
+    }
+
+}
---
+++ src/March_2025/Boolean_class.java
@@ -0,0 +1,12 @@
+package March_2025;
+
+public class Boolean_class {
+    public static void main(String[] args) {
+        String name ="Anuj";
+        String Surname ="Manekar";;
+        String Middlename= "Raj";
+        System.out.println("Name:"+ name);
+        System.out.println("Middle:"+ Middlename);
+        System.out.println("Surname:"+ Surname);
+    }
+}
