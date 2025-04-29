a37e94535ae3ed34eae0a2fa3b5e0ce3891f45a1
anujmanekar <anujmanekar96@gmail.com>
2025-04-29 23:54:55 +0530

    String

---
+++ src/April_2025/Name_Print.java
@@ -0,0 +1,15 @@
+package April_2025;
+
+public class Name_Print {
+    public static void main(String[] args) {
+        String name = "anuj";
+        String surname = "Manekar";
+        char new_line = '\n';
+        System.out.printf("My name is %s" + new_line + "last name is %s", name, surname);
+    }
+
+
+
+
+
+}
