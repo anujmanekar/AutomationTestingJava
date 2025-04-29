1c5ebfe3f45f1f9ea21b19614edd818a6237a1f7
anujmanekar <anujmanekar96@gmail.com>
2025-04-29 23:25:05 +0530

    Table of 9

--- src/April_2025/Table_of_9.java
+++ src/April_2025/Table_of_9.java
@@ -3,7 +3,21 @@ package April_2025;
 public class Table_of_9 {
     public static void main(String[] args) {
 
-        int num= 2;
-        System.out.printf("Value of %d", num);
+        int num= 9;
+
+        System.out.printf("%d * %d = %d", num, 1, num*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 2, num*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 3, num*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 4, num*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 5, num*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 6, num*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d", num, 7, num*7);
+
     }
 }
