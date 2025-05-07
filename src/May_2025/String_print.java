8177cc351b6a2ec2381b231989bd7870e23a282e
anujmanekar <anujmanekar96@gmail.com>
2025-05-07 22:40:02 +0530

    String

--- src/May_2025/String_print.java
+++ src/May_2025/String_print.java
@@ -4,7 +4,12 @@ public class String_print {
     public static void main(String[] args) {
         String str1 = "Anuj";
         String str2= "Manekar";
+        int a= 12;
+        int b= 543;
         System.out.println(str1);
         System.out.println(str2);
+        System.out.println(a);
+        System.out.println((b + a) +  str1  +  str2 );
+        System.out.println(str1 + str2 +  (a  +  b) );
     }
 }
