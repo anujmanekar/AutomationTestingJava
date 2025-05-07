c2c6cc3ad6d3cb5c684ae692d15ba8639ab3a009
anujmanekar <anujmanekar96@gmail.com>
2025-05-07 23:35:00 +0530

    Logical

---
+++ src/May_2025/Logical_operator.java
@@ -0,0 +1,14 @@
+package May_2025;
+
+public class Logical_operator {
+    public static void main(String[] args) {
+        boolean a= true;
+        boolean b= false;
+        boolean c = a || b;
+        boolean d = a || b;
+        System.out.println(d);
+        System.out.println(!b);
+
+        //Not of true in print c
+    }
+}
--- src/May_2025/relational_operators.java
+++ src/May_2025/relational_operators.java
@@ -3,6 +3,10 @@ package May_2025;
 public class relational_operators {
     public static void main(String[] args) {
         //Relational operators
+        int a= 2;
+        int b= 2;
 
+        boolean c= a > b;
+        System.out.println(c);
     }
 }
