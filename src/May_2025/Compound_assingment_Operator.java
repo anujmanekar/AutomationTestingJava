87aee3fa4916b76885cfd08bcbab340464aced2a
anujmanekar <anujmanekar96@gmail.com>
2025-05-07 23:07:09 +0530

    String

---
+++ src/May_2025/Compound_assingment_Operator.java
@@ -0,0 +1,12 @@
+package May_2025;
+
+public class Compound_assingment_Operator {
+    public static void main(String[] args) {
+        int a = 43;
+        int b = 4323;
+        a += 20;  //a= a+20
+        b -= 20;  //a= a-20
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
