21070384e0519569955129266b999182ee77bcaa
anujmanekar <anujmanekar96@gmail.com>
2025-05-07 23:15:24 +0530

    art

--- src/May_2025/Operators_03May.java
+++ src/May_2025/Operators_03May.java
@@ -2,11 +2,13 @@ package May_2025;
 
 public class Operators_03May {
     public static void main(String[] args) {
-   int a= +45;
-   int b= 45;
-   int c= -45;
-        System.out.println(a);
-        System.out.println(++b);
-        System.out.println(b);
+        // Arithmetic Operator
+// +,-,/,*
+        int a = 10;
+        int b = 2;
+        System.out.println(a+b); // + -> binary - 2 operands
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
     }
 }
---
+++ src/May_2025/relational_operators.java
@@ -0,0 +1,8 @@
+package May_2025;
+
+public class relational_operators {
+    public static void main(String[] args) {
+        //Relational operators
+
+    }
+}
