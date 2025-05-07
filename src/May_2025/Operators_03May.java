9c8c29b960ad28e84aa9ee4d7f45a1c31f23d0aa
anujmanekar <anujmanekar96@gmail.com>
2025-05-07 22:33:21 +0530

    String

--- src/May_2025/Operators_03May.java
+++ src/May_2025/Operators_03May.java
@@ -2,7 +2,11 @@ package May_2025;
 
 public class Operators_03May {
     public static void main(String[] args) {
-   int age= 45;
-        System.out.println(age);
+   int a= +45;
+   int b= 45;
+   int c= -45;
+        System.out.println(a);
+        System.out.println(++b);
+        System.out.println(b);
     }
 }
---
+++ src/May_2025/String_print.java
@@ -0,0 +1,10 @@
+package May_2025;
+
+public class String_print {
+    public static void main(String[] args) {
+        String str1 = "Anuj";
+        String str2= "Manekar";
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
